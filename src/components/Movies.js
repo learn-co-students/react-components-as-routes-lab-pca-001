69f9c6925b3f3d193679e40b23b409fade0ee0a1
Liam Tinney <ltinney@powerhrg.com>
2020-08-07 14:39:13 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,21 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {
+        movies.map(({title, time, genres}) => {
+          return (
+            <div className="movie">
+              <h3>Name: {title}</h3>
+              <h3>Time {time}</h3>
+              <h3>Genres:</h3>
+              <ul>
+                {genres.map(genre => <li>{genre}</li>)}
+              </ul>
+            </div>
+          )
+        })
+      }
     </div>
   );
 };
