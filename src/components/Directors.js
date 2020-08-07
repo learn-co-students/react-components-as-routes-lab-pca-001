2d21baacc23d3a7ebab112624e428968fa99d80e
Madison McCardle <madison.mccardle@powerhrg.com>
2020-08-07 11:40:56 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -5,6 +5,18 @@ const Directors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((director, index) => (
+          <div key={index}>
+          {console.log(director)}
+            <h3>Name: {director.name}</h3>
+            <p>Movies:</p>
+            <ul>
+              {director.movies.map(movie => <li> {movie} </li>)}
+            </ul>
+          </div>
+          )
+        )}
     </div>
   );
 }
